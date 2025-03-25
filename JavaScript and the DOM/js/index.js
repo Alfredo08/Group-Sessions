0b8f77fe5c6db075f670b02b14f12fc594da5d61
Alfredo08 <alfredosv08@gmail.com>
2025-03-25 12:54:02 -0600

    JS and the DOM

--- JavaScript and the DOM/js/index.js
+++ JavaScript and the DOM/js/index.js
@@ -1,26 +1,30 @@
-
-const removeConnection = (event) => {
-    event.target.closest('.card-list-item').remove();
-}
-
 let closeButtons = document.querySelectorAll('.close');
-console.log(closeButtons);
 
 for(let i = 0; i < closeButtons.length; i ++){
-    closeButtons[i].addEventListener('click', removeConnection);
+    closeButtons[i].addEventListener("click", function(event){
+        event.target.closest('.card-list-item').remove();
+        let connectionRequestNumber = Number(document.querySelector('#connectionRequestNumber').textContent);
+        document.querySelector('#connectionRequestNumber').textContent = connectionRequestNumber - 1;
+        if(connectionRequestNumber - 1 === 0){
+            alert("No more conexion requests!");
+        }
+    });
 }
 
+let cardHeaders = document.querySelectorAll('.card-header');
 
-const cardHeader = document.querySelector("#main-card-header");
-cardHeader.addEventListener('mouseenter', (event) => {
-    event.target.classList.add('extra');
-});
-cardHeader.addEventListener('mouseout', (event) => {
-    event.target.classList.remove('extra');
-});
+for(let i = 0; i < cardHeaders.length; i ++){
+    cardHeaders[i].addEventListener("mouseover", function(event){
+        event.target.classList.add('extra');
+    });
+    cardHeaders[i].addEventListener("mouseout", function(event){
+        event.target.classList.remove('extra');
+    });
+}
 
-const triggerSearch = document.querySelector('#trigger-search');
-triggerSearch.addEventListener('click', (event) => {
-    let searchContent = document.querySelector('#search').value;
-    alert("You are searching for " + searchContent);
+let searchButton = document.querySelector('#trigger-search');
+
+searchButton.addEventListener("click", function(event){
+    let searchValue = document.querySelector('#search').value;
+    alert(searchValue);
 });
