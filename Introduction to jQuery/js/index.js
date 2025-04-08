b585906208917dddc2283237116c5e78641d3949
Alfredo08 <alfredosv08@gmail.com>
2025-04-08 12:53:30 -0600

    Intro to jQuery

--- Introduction to jQuery/js/index.js
+++ Introduction to jQuery/js/index.js
@@ -1,26 +1,52 @@
 
-const removeConnection = (event) => {
-    event.target.closest('.card-list-item').remove();
-}
-
-let closeButtons = document.querySelectorAll('.close');
-console.log(closeButtons);
-
-for(let i = 0; i < closeButtons.length; i ++){
-    closeButtons[i].addEventListener('click', removeConnection);
-}
-
-
-const cardHeader = document.querySelector("#main-card-header");
-cardHeader.addEventListener('mouseenter', (event) => {
-    event.target.classList.add('extra');
-});
-cardHeader.addEventListener('mouseout', (event) => {
-    event.target.classList.remove('extra');
+$('#signout').on("click", function(){
+    console.log($(this));
+    alert("You are about to logout of our page. See you next time!");
 });
 
-const triggerSearch = document.querySelector('#trigger-search');
-triggerSearch.addEventListener('click', (event) => {
-    let searchContent = document.querySelector('#search').value;
-    alert("You are searching for " + searchContent);
+/*
+$("li").on("click", function(){
+    console.log($(this).text());
+});
+*/
+
+$('.close').on("click", function(){
+    $(this).closest('.card-list-item').remove();
+    let numOfRequest = $('#connectionRequestNumber').text();
+    numOfRequest --;
+    $('#connectionRequestNumber').text(numOfRequest);
+
+    if(numOfRequest === 0){
+        alert("No more connection requests!");
+    }
+});
+
+$('#search').on("keyup", function(){
+    console.log($(this).val());
+});
+
+$('#trigger-search').on("click", function(){
+    let newName = $('#search').val();
+    $('.card-list').prepend(`
+            <li class="card-list-item">
+                <span>
+                    <img src="images/todd-s.jpg" alt="todd" class="avatar-s">
+                    ${newName}
+                </span>
+                <span class="buttons">
+                    <img src="icons/accept-circle.png" alt="accept" class="icon">
+                    <img src="icons/close-circle.png" alt="close" class="icon close">
+                </span>
+            </li>
+        `);
+
+});
+
+
+$('.card-header').on("mouseenter", function(){
+    $(this).addClass('extra');
+});
+
+$('.card-header').on("mouseout", function(){
+    $(this).removeClass('extra');
 });
