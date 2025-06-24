3d998d429faa90e167bcd783161ab68c316f9842
Alfredo08 <alfredosv08@gmail.com>
2025-06-24 12:45:35 -0600

    React Bootstrap

--- React Bootstrap/bootstrap-in-react/src/App.jsx
+++ React Bootstrap/bootstrap-in-react/src/App.jsx
@@ -1,41 +1,74 @@
-import { Col, Container, Row, Form, Button } from "react-bootstrap";
-import './App.css';
+import { Col, Container, Row, Form, Button, Table } from 'react-bootstrap'
+import './App.css'
 
 const App = () => {
+
   return (
     <Container>
       <Row>
-        <Col lg={6}>
-          <h1> React bootstrap demonstration </h1>
-        </Col>
-        <Col lg={6}>
-          <p> A description will be here! </p>
+        <Col xs={12} className="border">
+          <h1> Bootstrap demonstration in React</h1>
         </Col>
       </Row>
       <Row>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+        <Col xs={6}>
+          <h2 className='text-primary'> Contact information </h2>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+        <Col xs={6}>
+        <h2 className='text-success'> Contact information </h2>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td colSpan={2}>Larry the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Col>
       </Row>
     </Container>
-  );
+  )
 }
 
-export default App;
+export default App
